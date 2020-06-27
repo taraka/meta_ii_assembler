bc2876899dc8065130d461e77f5f0523134d39d0
Tom Rawcliffe <git@tomrawcliffe.co.uk>
2020-06-27 23:30:42 +0100

    Working on assembler structure

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,81 @@
 use std::io::{self, Write, Read};
 use std::str;
-use std::collections::HashMap;
+use std::collections::{HashMap, LinkedList};
 
 struct Parser<'a> {
     text: &'a str,
-    labels: HashMap<String, usize>
+    labels: HashMap<String, usize>,
+    label_markers: LinkedList<LabelMarker>,
+    code: Vec<u8>
 }
 
+struct LabelMarker {
+    label: String,
+    addr: usize
+}
+
+enum Opcode {
+    ADR = 1
+}
 
 impl<'a> Parser<'a> {
     fn new(text: &str) -> Parser {
         Parser {
             text,
-            labels: HashMap::new()
+            labels: HashMap::new(),
+            label_markers: LinkedList::new(),
+            code: Vec::new()
         }
     }
 
-    fn parse(&self) {
-        for l in code.lines() {
+    fn parse(&mut self) {
+        for l in self.text.lines() {
             if l.chars().nth(0).unwrap() == '\t' {
-                parse_instruction(l);
+                self.parse_instruction(l);
             } else {
-                parse_label(l)
+                self.parse_label(l)
             }
         }
     }
 
-    fn parse_label(&self, l: &str) {
-        println!("{}", l);
+    fn parse_label(&mut self, l: &str) {
+        self.labels.insert(l.trim().to_string(), self.code.len());
     }
 
-    fn parse_instruction(&self, l: &str) {
-        println!("{}", l);
+    fn parse_instruction(&mut self, l: &str) {
+        let opcode = Parser::opcode(l.trim().split_whitespace().nth(0).unwrap());
+        match opcode {
+            Opcode::ADR => self.adr(l)
+        }
+    }
+
+    fn adr(&mut self, l: &str) {
+        self.code.push(Opcode::ADR as u8);
+        self.add_label_addr(l.trim().split_whitespace().nth(1).unwrap());
+    }
+
+    fn add_label_addr(&mut self, label: &str) {
+        let tmp_addr: usize = 0;
+
+        self.label_markers.push_back(LabelMarker {
+            label: label.to_string(),
+            addr: self.code.len()
+        });
+
+        for b in tmp_addr.to_le_bytes().iter() {
+            self.code.push(*b);
+        }
+
+    }
+
+    fn opcode(text: &str) -> Opcode {
+        match text {
+            "ADR" => Opcode::ADR,
+            _ => panic!(format!("Unknown opcode: {}", text))
+        }
     }
 }
+
 
 
 fn main() {
@@ -41,7 +83,7 @@ fn main() {
     io::stdin().read_to_end(&mut code_bytes);
 
     let code = str::from_utf8(&code_bytes[..]).unwrap();
-    let parser = Parser::new(&code);
+    let mut parser = Parser::new(&code);
     parser.parse()
 
 }
